e18398cdb52012c4e1dbabb6aba30a20068d90db
Dylan Baker <baker.dylan.c@gmail.com>
2015-07-02 14:39:39 -0700

    jsontidy.py: Allow reading from stdin and writing to stdout
    This adds additional functionality to the script, allowing it to read
    from stdin and write to stdout if no in input or output file is
    specified.

--- jsontidy.py
+++ jsontidy.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
-"""Simple python script to 'pretty' json files.
+"""Simple python script to 'pretty' json data.
 
 It attempts to make use of the C based simplejson, but will fall back to using
 the core json module if simplejson is not available.
+
+This script can read both data from stdin, or from a stored text file.
 
 """
 
 import argparse
 import os
+import sys
 
 try:
     import simplejson as json
@@ -18,15 +21,32 @@ except ImportError:
 
 def _parser():
     """Parse arguments and return an argparse namespace."""
+    # The use of type, default, and nargs allows the stdin/stdout to work,
+    # since one would either provide a file or stdin/out data
+
+    # FIXME: This will not allow stdin/file to be used. Is there an elegent way
+    # to solve that?
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='A JSON file to clean')
+    parser.add_argument('input',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin,
+                        nargs='?',
+                        help='JSON data to pretty. Default: stdin')
+    parser.add_argument('output',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        nargs='?',
+                        help='Location to place the updated data. '
+                             'Default: stdout')
     parser.add_argument('-i', '--indent',
                         type=int,
                         default=4,
                         help='The number of spaces to indent the file by')
     parser.add_argument('-p', '--preserve',
                         action='store_true',
-                        help='Save the original file as a <file>.bak')
+                        help='Save the original file as a <file>.bak, if '
+                             'overwritting. Has no effect if the destination '
+                             'is not the same as the input')
     return parser.parse_args()
 
 
@@ -34,17 +54,16 @@ def main():
     """Main function."""
     args = _parser()
 
-    # Read in the contents of the old file as json data
-    with open(args.file, 'r') as f:
-        content = json.load(f)
+    # Read in the contents of the old data as json data
+    content = json.load(args.input)
 
     # If requested save the old file with a .bak extension
-    if args.preserve:
-        os.rename(args.file, args.file + '.bak')
+    if (args.preserve and
+            os.path.abspath(args.input) == os.path.abspath(args.output)):
+        os.rename(args.input, args.input + '.bak')
 
-    # Finally dump the updated file
-    with open(args.file, 'w') as f:
-        json.dump(content, f, indent=args.indent)
+    # Finally dump the updated data
+    json.dump(content, args.output, indent=args.indent)
 
 
 if __name__ == '__main__':
